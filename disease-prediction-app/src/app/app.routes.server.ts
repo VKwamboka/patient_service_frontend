6a993440c0fc4ed8f87a6058904cf40633315921
VKwamboka <verahnyagaka101@gmail.com>
2026-05-20 11:27:50 +0000

    updated endpoints url

--- disease-prediction-app/src/app/app.routes.server.ts
+++ disease-prediction-app/src/app/app.routes.server.ts
@@ -1,6 +1,22 @@
+// import { RenderMode, ServerRoute } from '@angular/ssr';
+
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Prerender
+//   }
+// ];
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'login/:role',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'registration/:role',
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
